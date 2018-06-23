2a135078860779b9a38377969fd962acfceaa773
Isengar <ricci.gr@gmail.com>
2018-06-23 02:00:20 -0300

    creating dialog on floating button...

--- app/src/main/java/br/com/riccimac/fincontrol/ui/activity/TransactionListActivity.kt
+++ app/src/main/java/br/com/riccimac/fincontrol/ui/activity/TransactionListActivity.kt
@@ -1,14 +1,22 @@
 package br.com.riccimac.fincontrol.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import br.com.riccimac.fincontrol.R
+import br.com.riccimac.fincontrol.extensions.formatToBrazillianStandard
 import br.com.riccimac.fincontrol.model.Transaction
 import br.com.riccimac.fincontrol.model.Type
 import br.com.riccimac.fincontrol.ui.adapter.TransactionListAdapter
 import br.com.riccimac.fincontrol.ui.view.SummaryView
 import kotlinx.android.synthetic.main.activity_transaction_list.*
+import kotlinx.android.synthetic.main.form_transaction.view.*
 import java.math.BigDecimal
+import java.util.*
 
 class TransactionListActivity : AppCompatActivity() {
 
@@ -20,6 +28,46 @@ class TransactionListActivity : AppCompatActivity() {
 
         setupSummaryView(transactions)
         setupList(transactions)
+
+        list_transaction_add_income.setOnClickListener {
+            val decorView = window.decorView
+            val alertView = LayoutInflater.from(this)
+                            .inflate(R.layout.form_transaction,
+                                    decorView as ViewGroup,
+                                    false)
+
+            val year = 2018
+            val month = 4
+            val day = 23
+
+            val today = Calendar.getInstance()
+            alertView.form_transaction_date.setText(today.formatToBrazillianStandard())
+
+            alertView.form_transaction_date.setOnClickListener{
+                DatePickerDialog(this,
+                        DatePickerDialog.OnDateSetListener { _, year, month, day ->
+                            val dateSelected = Calendar.getInstance()
+                            dateSelected.set(year,month,day)
+                            alertView.form_transaction_date.setText(dateSelected.formatToBrazillianStandard())
+                        },
+                        year, month, day
+                ).show()
+            }
+
+            val adapterCategory = ArrayAdapter.createFromResource(this,
+                                                            R.array.category_income,
+                                                            android.R.layout.simple_spinner_dropdown_item)
+
+            alertView.form_transaction_category.adapter = adapterCategory
+
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.add_income)
+                    .setView(alertView)
+                    .setPositiveButton("Adicionar", null)
+                    .setNegativeButton("Cancelar", null)
+                    .show()
+        }
+
     }
 
     private fun setupSummaryView(transactions: List<Transaction>) {
